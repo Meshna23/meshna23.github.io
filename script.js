a7efa28feb3965eca4dcb1c17e0bc3f7158c98ae
Meshna23 <meshna.prasai@student.tdsb.on.ca>
2025-06-12 13:36:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // Model URL from Teachable Machine
 //**************************************************
 //* as before, paste your lnk below
-let URL = "https://teachablemachine.withgoogle.com/models/zKStqt6Ol/";
+let URL = "https://teachablemachine.withgoogle.com/models/yfMxI1NGx/";
 
 
 
@@ -231,7 +231,7 @@ async function playInstructionVideo() {
     if (model) {
         processFrame();
     } else {
-        console.log("https://teachablemachine.withgoogle.com/models/zKStqt6Ol/");
+        console.log("https://teachablemachine.withgoogle.com/models/yfMxI1NGx/");
     }
 }
 
